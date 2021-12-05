77c8f0a2e04ccad129c6cef0029df44d04c0b4d0
rodchen <rodchen_king@outlook.com>
2021-12-05 14:37:10 +0800

    1. update

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,4 +1,13 @@
+/*
+ * @Description: 
+ * @Author: rodchen
+ * @Date: 2021-12-05 14:28:02
+ * @LastEditTime: 2021-12-05 14:36:58
+ * @LastEditors: rodchen
+ */
 'use strict';
+
+const utils = require("@adapt-cli-dev/core")
 
 module.exports = core;
 
